11db3a4a6c2a21e1075400f787270d936dbe91be
relsa <relsample@gmail.com>
2017-12-22 05:43:33 +0900

    update Ex4.1

--- ch04/ex01/diffbit.go
+++ ch04/ex01/diffbit.go
@@ -10,14 +10,9 @@ func init() {
 
 func countDiffBit(ha, hb [32]byte) int {
 	var sum int
-	var diff [32]byte
 
 	for i := 0; i < 32; i++ {
-		diff[i] = ha[i] ^ hb[i]
-	}
-
-	for _, b := range diff {
-		sum += int(pc[b])
+		sum += int(pc[ha[i]^hb[i]])
 	}
 
 	return sum
