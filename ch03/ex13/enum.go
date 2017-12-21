d83a36f218900377bb5522cb3c5fd24997d2c737
relsa <relsample@gmail.com>
2017-12-22 05:41:34 +0900

    update Ex3.13

--- ch03/ex13/enum.go
+++ ch03/ex13/enum.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 const (
-	KB float64 = 1000
-	MB         = 1000 * KB
-	GB         = 1000 * MB
-	TB         = 1000 * GB
-	PB         = 1000 * TB
-	EB         = 1000 * PB
-	ZB         = 1000 * EB
-	YB         = 1000 * ZB
+	KB = 1000
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
 )
 
 func main() {
@@ -20,6 +20,6 @@ func main() {
 	fmt.Println(TB)
 	fmt.Println(PB)
 	fmt.Println(EB)
-	fmt.Println(ZB)
-	fmt.Println(YB)
+	// fmt.Println(ZB)
+	// fmt.Println(YB)
 }
